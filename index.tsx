0ecaddafbfffb1186434d68eef8b731ec82c9ddf
Betroxen <encryptedcryptowallet@gmail.com>
2025-11-15 13:51:56 +0200

    Update index.tsx

--- index.tsx
+++ index.tsx
@@ -1,38 +1,21 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import { AppProvider } from './context/AppContext';
 import { ToastProvider } from './context/ToastContext';
-
-function Root() {
-  const [basename, setBasename] = useState('/');
-
-  useEffect(() => {
-    // Extract first non-empty path segment as dynamic basename
-    const pathSegments = window.location.pathname.split('/').filter(Boolean);
-    setBasename(pathSegments.length > 0 ? `/${pathSegments[0]}` : '/');
-  }, []);
-
-  return (
-    <BrowserRouter basename={basename} key={basename}>
-      <AppProvider>
-        <ToastProvider>
-          <App />
-        </ToastProvider>
-      </AppProvider>
-    </BrowserRouter>
-  );
-}
 
 const rootElement = document.getElementById('root');
 if (!rootElement) {
   throw new Error("Could not find root element to mount to");
 }
-const root = ReactDOM.createRoot(rootElement);
 
+const root = ReactDOM.createRoot(rootElement);
 root.render(
   <React.StrictMode>
-    <Root />
+    <AppProvider>
+      <ToastProvider>
+        <App />
+      </ToastProvider>
+    </AppProvider>
   </React.StrictMode>
 );
