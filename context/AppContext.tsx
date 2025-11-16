a0a13bcb7e83b6a7777b37086ed3e737fbb99f36
Betroxen <encryptedcryptowallet@gmail.com>
2025-11-16 18:29:15 +0200

    Update AppContext.tsx

--- context/AppContext.tsx
+++ context/AppContext.tsx
@@ -1,5 +1,5 @@
 import React, { createContext, useState, ReactNode, useContext, useCallback } from 'react';
-import { useAppwriteAuth } from './AppwriteAuthContext'; // Using the real auth hook
+import { useAppwriteAuth } from './AppwriteAuthContext'; // <--- NEW: Import the real auth hook
 
 // --- START: TYPE DEFINITIONS ---
 
